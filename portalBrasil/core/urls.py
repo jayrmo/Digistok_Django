1131458f4832072938aeb7dcf80c616cbd2cf907
Jayrmo <jayr.jm7@gmail.com>
2025-05-12 19:42:50 -0300

    Estruturação Pastas

---
+++ portalBrasil/core/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from .views import * #importa todas as funções de dentro do arquivo views
+
+urlpatterns = [
+    path('', homepage, name='homepage'),#cria o link da página homepage(q é def em views).O name é um identificador q aponta para esse caminho dentro da ''.Nesse caso o caminho é o dominio puro..
+]
