e5682ada80494e0675d226661a3276b64bd11bc7
Jayrmo <jayr.jm7@gmail.com>
2025-05-13 17:27:14 -0300

    após página de login

--- portalBrasil/core/urls.py
+++ portalBrasil/core/urls.py
@@ -1,7 +1,13 @@
 
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import * #importa todas as funções de dentro do arquivo views
+from .views import * 
 
 urlpatterns = [
-    # path('', homepage, name='homepage'),#cria o link da página homepage(q é def em views).O name é um identificador q aponta para esse caminho dentro da ''.Nesse caso o caminho é o dominio puro..
+    # path('noticia/login/', login, name='login'),
+    path('login/', LoginView.as_view(),
+name='login'),
+    path('login/', auth_views.LogoutView.as_view(), name='logout'),
+
+
 ]
--- portalBrasil/core/views.py
+++ portalBrasil/core/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from .models import *
+import datetime
 
 # Create your views here.
+
+# def login(request):
+#     context = {
+#         'title':'Login'
+#     }
+#     return render(request, 'login.html',context)
+
+
+class LoginView(View):
+    def get(self, request):
+        print(f'dentro do get')
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print('dentro do post')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            # views.py
+            messages.error(request, 'Usuário ou senha inválidos')
+            if messages.get_messages(request):
+                first_message = list(messages.get_messages(request))[0]
+            context = {'first_message': first_message}
+            print(f'dentro do else{username}-{password}')
+            return render(request, 'login.html', context)
