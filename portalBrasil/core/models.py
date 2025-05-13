0ce6d72b8aa8baaab10788ed9267477050261a37
Jayrmo <jayr.jm7@gmail.com>
2025-05-12 23:22:41 -0300

    após core

--- portalBrasil/core/models.py
+++ portalBrasil/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+    
+    
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255, blank=True)
+    conteudo = models.TextField()
+    autor = models.CharField(max_length=100, blank=True)
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    fonte = models.CharField(max_length=255, blank=True)
+    local = models.CharField(max_length=100, blank=True)
+    imagem_url = models.ImageField(null=True,blank=True)
+    categoria = models.ForeignKey(Categoria, null=True,blank=True,on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.titulo
+
+
--- portalBrasil/core/urls.py
+++ portalBrasil/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import * #importa todas as funções de dentro do arquivo views
 
 urlpatterns = [
-    path('', homepage, name='homepage'),#cria o link da página homepage(q é def em views).O name é um identificador q aponta para esse caminho dentro da ''.Nesse caso o caminho é o dominio puro..
+    # path('', homepage, name='homepage'),#cria o link da página homepage(q é def em views).O name é um identificador q aponta para esse caminho dentro da ''.Nesse caso o caminho é o dominio puro..
 ]
